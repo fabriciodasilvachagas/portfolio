6b744b9e9b3a34fd3e63a5b18dfbac3f526778e4
fabriciodasilvachagas <fabriciolg410@gmail.com>
2026-06-28 14:50:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,24 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sobre mim - Fabricio Silva</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+function validacao(){
+    //pegando os valores dos inputs de contato.html
+    let nome = document.getElementById("nome").value
+    let email = document.getElementById("email").value
+    let mensagem = document.getElementById("mensagem").value
 
-    <!--Menu de navegação-->
-    <nav>
-        <!--Botao para armazenar as opções em caso de telas menores-->
-        <button class="menu-toggle" id="menu-toggle">☰</button>
+    //para validação do e-mail
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        <ul class="menu" id="menu">
-            <li><a href="index.html">Sobre mim</a></li>
-            <li><a href="formacao.html">Formação</a></li>
-            <li><a href="portfolio.html">Portfólio</a></li>
-            <li><a href="contato.html">Contato</a></li>
-        </ul>
-    </nav>
+    //validacao
+    if(!nome || !mensagem){
+        alert("Preencha todos os dados corretamente!")
+    }
+    
+    if (!regex.test(email)) {
+        alert("Digite um e-mail válido!");
+    }else{
+        alert("Formulário enviado com sucesso!")
+        nome.value = ""
+        email.value = ""
+        mensagem.value = ""
+    }
 
-    <!--Imagem de apresentação-->
-    <img src="imgs/perfil.jpg" alt="perfil" id="perfil">
-
-    <!--Breve resumo sobre mim-->
-    <p id="apresentacao">Olá! Meu nome é Fabricio Silva, tenho 23 anos e sou programador. 
-    As linguagens de programação que mais domino são JavaScript, Python e PHP.
-    Já programei alguns sites bem legais como um chat feito em NodeJS em que cada
-    usuário tem seu perfil e trocam mensagens entre si em tempo real usando WebSocket.
-    No meu tempo livre gosto de praticar esportes e assistir séries e filmes.
-    </p>
-<script>
-    const toggle = document.getElementById("menu-toggle");
-    const menu = document.getElementById("menu");
-
-    toggle.addEventListener("click", () => {
-
-        menu.classList.toggle("active");
-
-        if(menu.classList.contains("active")){
-            toggle.innerHTML = "✕";
-            toggle.style.color = "white"
-            toggle.style.backgroundColor = "#0d47a1"
-        }else{
-            toggle.innerHTML = "☰";
-            toggle.style.color = "#0d47a1"
-            toggle.style.backgroundColor = "white"
-        }
-
-    });
-</script>
-</body>
-</html>
+}
